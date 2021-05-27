3e0c607c0d8985334a3d2e4e9a35965c11bc7305
omar <omar.alshyokh@gmail.com>
2021-05-28 00:55:35 +0300

    Initial Commit

---
+++ Trekr/TrekrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekrApp.swift
+//  Trekr
+//
+//  Created by omar on 5/28/21.
+//
+
+import SwiftUI
+
+@main
+struct TrekrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
